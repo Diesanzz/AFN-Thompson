9bbd280b108fa99ba0264a7d662b27e9c1ba4113
Dai <diesanz06@gmail.com>
2026-09-06 18:33:16 -0600

    Permite mostrar el AFN y sus transiciones

--- interfaz.py
+++ interfaz.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import messagebox
+import afn as AFN
+from tkinter import messagebox, ttk
 
 from afn import AFN
 
@@ -75,7 +76,8 @@ class InterfazThompson:
         boton_ver = tk.Button(
             self.ventana,
             text="Ver AFN",
-            width=25
+            width=25,
+            command=self.ventana_ver_afn
         )
 
         boton_ver.pack(pady=5)
@@ -169,6 +171,169 @@ class InterfazThompson:
         )
 
         boton_crear.pack(pady=5)
+
+    def ventana_ver_afn(self):
+
+        # Verificamos que existe al menos un AFN
+        ids = AFN.obtener_ids()
+
+        if not ids:
+            messagebox.showwarning(
+                "Sin AFN",
+                "Primero debes crear al menos un AFN"
+            )
+            return
+
+        ventana = tk.Toplevel(self.ventana)
+
+        ventana.title("Ver AFN")
+        ventana.geometry("650x650")
+
+        titulo = tk.Label(
+            ventana,
+            text="Ver AFN",
+            font=("Arial", 16, "bold")
+        )
+
+        titulo.pack(pady=15)
+
+        #Aqui implementamos un selector de AFN
+
+        frame_selector = tk.Frame(ventana)
+        frame_selector.pack(pady=10)
+
+        tk.Label(
+            frame_selector,
+            text="AFN:"
+        ).pack(side="left", padx=5)
+
+        selector_afn = ttk.Combobox(
+            frame_selector,
+            values=ids,
+            state="readonly",
+            width=25
+        )
+
+        selector_afn.pack(side="left", padx=5)
+
+        selector_afn.current(0)
+
+        # Mostramos la info general
+
+        frame_info = tk.Frame(ventana)
+        frame_info.pack(pady=10)
+
+        label_inicial = tk.Label(frame_info, text="")
+        label_inicial.pack()
+
+        label_estados = tk.Label(frame_info, text="")
+        label_estados.pack()
+
+        label_aceptacion = tk.Label(frame_info, text="")
+        label_aceptacion.pack()
+
+        label_alfabeto = tk.Label(frame_info, text="")
+        label_alfabeto.pack()
+
+
+        tabla = ttk.Treeview(
+            ventana,
+            columns=("origen", "simbolo", "destino"),
+            show="headings",
+            height=12
+        )
+
+        tabla.heading("origen", text="Estado origen")
+        tabla.heading("simbolo", text="Simbolo")
+        tabla.heading("destino", text="Estado destino")
+
+        tabla.column(
+            "origen",
+            width=150,
+            anchor="center"
+        )
+
+        tabla.column(
+            "simbolo",
+            width=150,
+            anchor="center"
+        )
+
+        tabla.column(
+            "destino",
+            width=150,
+            anchor="center"
+        )
+
+        tabla.pack(pady=15)
+
+        # Con esta funcion mostraremos el AFN
+
+        def mostrar_afn(event=None):
+
+            id_afn = selector_afn.get()
+
+            afn = AFN.obtener_afn(id_afn)
+
+            if afn is None:
+                return
+
+            estados = sorted(
+                afn.edos_afn,
+                key=lambda e: e.id_edo
+            )
+
+            aceptacion = sorted(
+                afn.edos_acept,
+                key=lambda e: e.id_edo
+            )
+
+            label_inicial.config(
+                text=f"Estado inicial: {afn.edo_ini.id_edo} "
+            )
+
+            label_estados.config(
+                text="Estados: " +
+                str([e.id_edo for e in estados])
+            )
+
+            label_aceptacion.config(
+                text="Estados de aceptacion: " +
+                str([e.id_edo for e in aceptacion])
+            )
+
+            label_alfabeto.config(
+                text="Alfabeto: " +
+                str(sorted(afn.alfabeto))
+            )
+
+            # Este pa limpiar la tabla anterior
+            for fila in tabla.get_children():
+                tabla.delete(fila)
+
+            for origen, simbolo, destino in afn.obtener_transiciones():
+
+                tabla.insert(
+                    "",
+                    "end",
+                    values=(
+                        origen,
+                        simbolo,
+                        destino
+                    )
+                )
+
+            # Con este selector se cambia 
+            # automaticamente al cambiar de AFN
+
+        selector_afn.bind(
+            "<<ComboboxSelected>>",
+            mostrar_afn
+        )
+
+        mostrar_afn()
+
+
 
     def ejecutar(self):
         self.ventana.mainloop()
